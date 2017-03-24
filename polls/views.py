c60e0725f28a8535427147c0e4517aa1867a3967
Ayush <ayushjn20.uec2016@iitr.ac.in>
2017-03-24 15:34:42 +0530

    hello detail.html, hardcoded urls-->bye bye

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import RequestContext, loader
 from polls.models import Question
+def detail(request, question_id):
+	try:
+		question = Question.objects.get(pk=question_id)
+	except Question.DoesNotExist:
+		raise Http404("Question does not exist")
+	return render(request, 'polls/detail.html', {'question': question})
+#	return HttpResponse("Question no. is %s." % question_id)
 
 def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
 	context = {'latest_question_list': latest_question_list}
 	return render(request, 'polls/index.html', context)
-
-
-def detail(request, question_id):
-	return HttpResponse("Question no. is %s." % question_id)
 
 def results(request, question_id):
 	response = "result of question is %s."
@@ -18,3 +21,4 @@ def results(request, question_id):
 
 def vote(request, question_id):
 	return HttpResponse("You're voting on question %s." % question_id)
+
